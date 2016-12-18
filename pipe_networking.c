ae0dd505f61a7525923170873c761474b0fc4890
Yikai <ywang8@stuy.edu>
2016-12-18 15:11:02 -0500

    working on pipes

--- pipe_networking.c
+++ pipe_networking.c
@@ -5,29 +5,56 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include "pipe_networking.h"
+
 int server_handshake(int * from_client) {
   // make the named pipe
   mkfifo("luigi",0644);
   printf("[SERVER] WPK created\n");
   // open (connect) to it
-  int fd = open("luigi",O_WRONLY);
-  printf("%s\n", );
-  // write a string literal to it
-  char buff[512];
-  read(fd, &buff, sizeof(buff));
-  printf("[SERVER] re%s\n", );
+  printf("[SERVER] connection attempt\n");
+  *from_client = open("luigi",O_RDONLY);
 
-  return 0;
+  //attempt to read
+  char buffer[512];//gets client fifo name
+  read(*from_client, buffer, sizeof(buffer));
+  printf("[SERVER] recieved: %s\n", );
+
+  //remove pipe
+  execlp("rm", "rm", "-f", "luigi", NULL);
+
+  //connect to client pipe
+  int clientPipe = open(buffer, O_WRONLY);
+  printf("[SERVER] connected to client FIFO\n", );
+
+  //write to client
+  char * message = "Hello, client!";
+  write(clientPipe, message, strlen(message));
+
+  //recieve confirmation of connection
+  read(*from_client, buffer, sizeof(buffer));
+  printf("[SERVER] recieved: %s\n", buffer);
+
+  return clientPipe;
 }
 
-int client_handshake(int * to_client) {
+int client_handshake(int * to_server ) {
   // make the named pipe
-  mkfifo("peach",0644);
+  char clientPipe[512];
+  sprintf(clientPipe, "%d", getpid());
+  mkfifo(clientPipe,0644);
+  printf("[CLIENT] fifo created created: %s\n", clientPipe );
+
   // open (connect) to it
-  int fd = open("peach",O_WRONLY);
-  printf("[CLIENT] fifo created created: %d\n", getpid() );
+  *to_server = open("luigi",O_WRONLY);
+  printf("[CLIENT] connected to WKP\n" );
+
   // write a string literal to it
-  write(fd,"hello\n",13);
+  write(*to_server, clientPipe, strlen(clientPipe));
+  printf("[CLIENT] sent message\n" );
+
+  //connect to server fifo
+  int 
 
   return 0;
 }
---
+++ pipe_networking.h
@@ -0,0 +1,2 @@
+int server_handshake(int * from_client);
+int client_handshake(int * to_server);
